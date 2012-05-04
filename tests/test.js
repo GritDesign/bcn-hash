98aba305842797d06b956e78a5679eb2d46e5833
Mark Stewart <mark@grit-design.com>
2012-05-04 10:51:13 -0400

    fixed test case

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,11 @@
-var bcnHash = require("..");
+var hash = require("..");
 var assert = require("assert");
+var stringify = require("bcn-stringify").stringify;
 
 var cases = require("./test_cases.js").cases;
 
 for (var i=0; i<cases.length; i++) {
-    var hash = bcnHash.hashObject(cases[i][0]);
-    console.log(hash);
+    var json = stringify(cases[i][0]); 
+    var id = h.hashString(json);
+    console.log(id);
 }
